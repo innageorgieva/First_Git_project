fe39f67b60035649a18bb351b399c8e41781ed13
Inna <innageorgievainna@gmail.com>
2024-12-14 17:03:46 -0500

    Initial commit with gitignore and first class

--- src/day01_commin_push/First.java
+++ src/day01_commin_push/First.java
@@ -3,5 +3,6 @@ package day01_commin_push;
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello from the first class");
+        System.out.println("New line in first class");
     }
 }
