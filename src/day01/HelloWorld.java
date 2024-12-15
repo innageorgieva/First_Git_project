e353ff8d287003ef7c6659b9d9701420ffcd4077
innageorgieva <innageorgievainna@gmail.com>
2024-12-15 09:38:48 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,4 +1,9 @@
 package day01;
 
 public class HelloWorld {
+  Systems.out.println("Hello World");
+  Systems.out.println("Hello Cydeo");
+  Systems.out.println("Hello Programmers");
+  Systems.out.println("Hello World");
+  Systems.out.println("Hello World");
 }
